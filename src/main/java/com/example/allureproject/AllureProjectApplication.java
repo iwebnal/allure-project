01e93ba812eac64aeb6ebe4e1fed1db95a6689ad
Akhmedkhan.Khatukhov <Akhmedkhan.Khatukhov@rt.ru>
2022-11-16 14:23:53 +0300

    first commit

---
+++ src/main/java/com/example/allureproject/AllureProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.allureproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllureProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllureProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/allureproject/AllureProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allureproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllureProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
